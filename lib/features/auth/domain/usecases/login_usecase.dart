99e691032381ec226ecd29171c471bdec8b0120e
sthaasis007 <aashishshresth123@gmail.com>
2026-01-02 02:14:00 +0545

    made changes

---
+++ lib/features/auth/domain/usecases/login_usecase.dart
@@ -0,0 +1,39 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lost_n_found/core/error/failures.dart';
+import 'package:lost_n_found/core/usecases/app_usecase.dart';
+import 'package:lost_n_found/features/auth/data/repositories/auth_repository.dart';
+import 'package:lost_n_found/features/auth/domain/entities/auth_entity.dart';
+import 'package:lost_n_found/features/auth/domain/repositories/auth_repositories.dart';
+
+class LoginUsecaseParams extends Equatable {
+
+  final String email;
+  final String password;
+  const LoginUsecaseParams({
+    required this.email,
+    required this.password,
+  });
+  @override
+  List<Object?> get props => [email, password];
+}
+
+// prvider for login usecase
+final loginUsecaseProvider = Provider<LoginUsecase>((ref) {
+  final authRepository = ref.read(authRepositoryProvider);
+  return LoginUsecase(authRepository: authRepository);
+});
+    
+
+class LoginUsecase implements UsecaseWithParms<AuthEntity, LoginUsecaseParams> {
+ 
+  final IAuthRepository _authRepository;
+  LoginUsecase({required IAuthRepository authRepository})
+    : _authRepository = authRepository;
+  @override
+  Future<Either<Failure, AuthEntity>> call(LoginUsecaseParams params) {
+    return _authRepository.login(params.email, params.password);
+  }
+
+} 
